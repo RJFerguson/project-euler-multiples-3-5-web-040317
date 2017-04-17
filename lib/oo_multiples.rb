de95cba191536ede5cd4446dbd232a2e23fd6d8a
Ryan Ferguson <fergusman@gmail.com>
2017-04-17 11:06:57 -0400

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,26 @@
-# Enter your object-oriented solution here!
+# Enter your object-oriented solution hereC
+
+class Multiples
+    attr_accessor :limit 
+
+    def initialize(limit)
+        @limit = limit 
+    end 
+
+    def collect_multiples
+        i = 1 
+        arr = [] 
+
+        while i < limit 
+            if (i % 3 == 0) || (i % 5 == 0 )
+            arr << i 
+            end 
+            i += 1 
+        end 
+        arr 
+    end 
+
+    def sum_multiples
+        collect_multiples.inject(:+) 
+    end 
+end 
